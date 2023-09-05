988fcbd1299061d2ec4510c9e91c35515cc33c9a
matthewkuria <mathewkwachira@gmail.com>
2023-09-05 23:49:40 +0300

    New commit background properties changed

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025","blue","violet","white","indigo"];
 const btn = document.getElementById("btn");
 const color= document.querySelector(".color");
+const bcolor = document.querySelector(".bcolor");
+const bcolor1 = document.querySelector(".bcolor1");
+const divElement = document.getElementById("hero");
+const divElement1 = document.getElementById("hero1");
 
 btn.addEventListener('click',function(){
 	//get random number between 0 and 3
 	const randomNumber = getRandomNumber();
-	document.body.style.backgroundColor = colors[randomNumber];
+	document.body.style.backgroundColor = color[randomNumber];
 	color.textContent = colors[randomNumber];
+	bcolor.textContent = colors[randomNumber];
+	bcolor1.textContent = colors[randomNumber];
+	divElement.style.backgroundColor = colors[randomNumber];
+	divElement1.style.backgroundColor = colors[randomNumber];
 });
 
 
